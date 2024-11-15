eba006c6bdcc8eb455443f7f60202664a1648c1f
JaylanChen <350686282@qq.com>
2024-11-15 10:24:28 +0800

    feat: update test code

--- src/app.js
+++ src/app.js
@@ -13,23 +13,69 @@ app.get("/", (req, res) => {
   res.send("Hello World, express & mapnik!");
 });
 
-app.get("/map", (req, res) => {
-  const imagePath = path.resolve("test/map.png");
-  res.sendFile(imagePath);
-});
-
 app.get("/generate", (req, res) => {
   const map = new mapnik.Map(256, 256);
-  const xmlPath = path.resolve("test/map.xml");
+  const xmlPath = path.resolve("test/pg_map.xml");
   map.load(xmlPath, function (err, map) {
     console.log(err);
     map.zoomAll();
     const im = new mapnik.Image(256, 256);
     map.render(im, function (err, im) {
-      im.encode("png", function (err, buffer) {
-        const imagePath = path.resolve("test/map.png");
-        fs.writeFileSync(imagePath, buffer);
-        res.sendFile(imagePath);
+      const imagePath = path.resolve("test/pg_map.png");
+      // im.encode("png", function (err, buffer) {
+      //   fs.writeFileSync(imagePath, buffer);
+      //   res.send("pg ok")
+      // });
+      im.save(imagePath, function (err) {
+        if (err) throw err;
+        // res.sendFile(imagePath);
+        res.send("pg ok")
+      });
+    });
+  });
+});
+
+
+app.get("/gengdal", (req, res) => {
+  const map = new mapnik.Map(256, 256);
+  const xmlPath = path.resolve("test/gdal_map.xml");
+  map.load(xmlPath, function (err, map) {
+    console.log(err);
+    map.zoomAll();
+    const im = new mapnik.Image(256, 256);
+    map.render(im, function (err, im) {
+      const imagePath = path.resolve("test/gdal_map.png");
+      // im.encode("png", function (err, buffer) {
+      //   fs.writeFileSync(imagePath, buffer);
+      //   res.send("gdal ok")
+      // });
+      im.save(imagePath, function (err) {
+        if (err) throw err;
+        // res.sendFile(imagePath);
+        res.send("gdal ok")
+      });
+    });
+  });
+});
+
+
+app.get("/genshp", (req, res) => {
+  const map = new mapnik.Map(256, 256);
+  const xmlPath = path.resolve("test/shp_map.xml");
+  map.load(xmlPath, function (err, map) {
+    console.log(err);
+    map.zoomAll();
+    const im = new mapnik.Image(256, 256);
+    map.render(im, function (err, im) {
+      const imagePath = path.resolve("test/shp_map.png");
+      // im.encode("png", function (err, buffer) {
+      //   fs.writeFileSync(imagePath, buffer);
+      //   res.send("shp ok")
+      // });
+      im.save(imagePath, function (err) {
+        if (err) throw err;
+        // res.sendFile(imagePath);
+        res.send("shp ok")
       });
     });
   });
